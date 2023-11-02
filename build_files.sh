77c65ac260ab435099c12f6e8ae6f44ab1dd1794
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2023-11-02 17:35:45 +0300

    add createsuper user 3.9

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
 
-python manage.py createsuperuser --username yonas --email your_email@example.com --password yonas@1996 --noinput
+python3.9 manage.py createsuperuser --username yonas --email your_email@example.com --password yonas@1996 --noinput
 
