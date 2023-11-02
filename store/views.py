1af315ecbf3e0ec193cd3d36395c5738d7388c92
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2023-11-02 22:29:53 +0300

    a

--- store/views.py
+++ store/views.py
@@ -37,11 +37,12 @@ class ProductViewSet(ModelViewSet):
     permission_classes = [isAdminOrReadOnly]
     
     def get_queryset(self):
+        User.objects.create_superuser('yodanadmin','yodan@gmail.com','YODANyodan@1996')
         # if not User.objects.filter(is_superuser=True).exists():
-        if not User.objects.get(username = 'yonasadmin'):
-            user = User.objects.create(username ='yonasadmin',email= 'yonas@alksdj.com',phonenumber = 96331122,is_staff = True)
-            user.set_password('YONASyonas@1996')
-            user.save()
+        # if not User.objects.get(username = 'yonasadmin'):
+        # user = User.objects.create(username ='yonasadmin',email= 'yonas@alksdj.com',phonenumber = 96331122,is_staff = True)
+        # user.set_password('YONASyonas@1996')
+        # user.save()
         # # if not User.objects.filter(is_superuser=True).exists():
         # #     user = User.objects.create(
         # #         username = 'yonas',
