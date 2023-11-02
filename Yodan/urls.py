12bc70b344e58b13586198a83ea423b3659c4178
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2023-11-02 13:04:12 +0300

    test

--- Yodan/urls.py
+++ Yodan/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
-# # add at the last
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
